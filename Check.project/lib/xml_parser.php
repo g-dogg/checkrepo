69d3c62dac781cd7b79c0fea8fe78f326c0db337
Ghost Dogg <g_dogg@xakep.ru>
2013-06-17 16:55:37 +0700

    modified:   xml_parser.php 	modified:   ../index.php 	modified:   mysql.php

--- Check.project/lib/xml_parser.php
+++ Check.project/lib/xml_parser.php
@@ -3,46 +3,57 @@
 include 'mysql.php';
 
 echo'<table border=1>';
-if(file_exists('../xml/sud3005.xml'))
-    {
-        $xml = simplexml_load_file('../xml/sud3005.xml');
-    }
-else 
-    {
-    exit("Файл отсуствует!");
-    }
-/* в цикле перебираем все параметры, указаные в xml файле
- * причем теги <schedule></schedule> и все, что выше - пропускаем
- */
-  
-foreach ($xml->session as $sr) 
-    {
-    $zal = $sr->sessionRoom;
-    $sDate = $sr->sessionDate;
-    $sTime = $sr->beginTime;
-    $sNumber = $sr->sessionNumber;
-    $judge = $sr->judge;
 
-    foreach($sr->sides as $sds)
+$dirHabdle = glob("../xml/*.xml", GLOB_NOSORT);
+print_r($dirHabdle);
+$countFiles = count($dirHabdle);
+$arrayElement = $countFiles - 1;
+echo "Будет обработано " . $countFiles . " файла(ов)<br />";
+
+for($i=0; $i<=$arrayElement; $i++)
+{
+
+    if(file_exists($dirHabdle[$i]))
         {
-        $sType = $sds->sideType;
-        $sName = htmlspecialchars($sds->sideName);
-
-    //echo "Место " . $zal . "<br />Дата" . $sdate . " Время начала заседания" . $stime . "<br />";
-  
-    echo '<tr>
-              <td>',$zal,'</td>
-              <td>',$sDate,'</td>
-              <td>',$sTime,'</td>
-              <td>',$sNumber,'</td>
-              <td>',$judge,'</td>
-              <td>',$sType,'</td>
-              <td>',$sName,'</td>    
-         </tr>';
-    $sql_query = "INSERT INTO `hear` (`hearingPoint`, `hearingDate`, `hearingTime`, `caseNum`, `Judge`, `Part`, `partType`) VALUES ('$zal', '$sDate', '$sTime', '$sNumber', '$judge', '$sName', '$sType')";
-    $result = mysql_query($sql_query);
+            $xml = simplexml_load_file($dirHabdle[$i]);
         }
+    else 
+        {
+            exit("Файл отсуствует!");
+        }
+        /* в цикле перебираем все параметры, указаные в xml файле
+        * причем теги <schedule></schedule> и все, что выше - пропускаем
+        */
+  
+        foreach ($xml->session as $sr) 
+         {
+            $zal = $sr->sessionRoom;
+            $sDate = $sr->sessionDate;
+            $sTime = $sr->beginTime;
+            $sNumber = $sr->sessionNumber;
+            $judge = $sr->judge;
+
+            foreach($sr->sides as $sds)
+            {
+                $sType = $sds->sideType;
+                $sName = htmlspecialchars($sds->sideName);
+
+                //echo "Место " . $zal . "<br />Дата" . $sdate . " Время начала заседания" . $stime . "<br />";
+  
+                echo '<tr>
+                          <td>',$zal,'</td>
+                          <td>',$sDate,'</td>
+                          <td>',$sTime,'</td>
+                          <td>',$sNumber,'</td>
+                          <td>',$judge,'</td>
+                          <td>',$sType,'</td>
+                          <td>',$sName,'</td>    
+                     </tr>';
+                $sql_query = "INSERT INTO `hear` (`hearingPoint`, `hearingDate`, `hearingTime`, `caseNum`, `Judge`, `Part`, `partType`) VALUES ('$zal', '$sDate', '$sTime', '$sNumber', '$judge', '$sName', '$sType')";
+                $result = mysql_query($sql_query);
+            }
          
-    }
+        }
+}
 echo '</table>';
 ?>
